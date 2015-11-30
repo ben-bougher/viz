412fbe018eb48589b102f12f3105932ed67390d8
Ben Bougher <ben.bougher@gmail.com>
2015-11-29 18:51:23 -0800

    finished

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+
+
 # -*- coding: utf-8 -*-
 # modelr web app
 # Agile Geoscience
@@ -14,13 +15,19 @@ import numpy as np
 
 
 env = Environment(loader=FileSystemLoader(join(dirname(__file__),
-                                            'templates')))
+                                               'templates')))
 
 
 class Viz(webapp2.RequestHandler):
 
-    attributes = ['amplitude', 'phase', 'similarity',
-                  'energy', 'intercept', 'gradient']
+    attributes = ['FI', 'similarity',
+                  'amplitude', 'intercept', 'gradient']
+
+    file_dict = {"similarity": "cont.npy",
+                 "amplitude": "amp_slice.npy",
+                 "gradient": "gradient.npy",
+                 "intercept": "intercept.npy",
+                 "FI": "FI.npy"}
 
 
 class MainHandler(Viz):
@@ -40,13 +47,35 @@ class DataHandler(Viz):
 
         self.response.headers['Content-Type'] = 'application/json'
 
+        A = np.flipud(np.nan_to_num(np.load('amp_slice.npy'))).T
+        C = np.flipud(np.nan_to_num(np.load('cont.npy'))).T
+        A /= np.amax(np.abs(A))
+
+        gradient = np.flipud(np.nan_to_num(np.load('gradient.npy'))).T
+        intercept = np.flipud(np.nan_to_num(np.load('intercept.npy'))).T
+        FI = np.flipud(np.nan_to_num(np.load('FI.npy'))).T
         output = {}
         output["attributes"] = self.attributes
 
         output["data"] = []
 
-        for point in range(500):
-            output["data"].append({attr: np.random.randn() for attr in self.attributes})
+        output["attr1"] = A.tolist()
+        output["attr2"] = C.tolist()
+        output["cbar1"] = np.linspace(-1, 1, 10).tolist()
+        output["cbar2"] = (1 - np.linspace(0.0, 1, 10)).tolist()[::-1]
+
+        for amp, cont, grad, inter, fi in zip(A.flatten()[::25],
+                                              C.flatten()[::25],
+                                              gradient.flatten()[::25],
+                                              intercept.flatten()[::25],
+                                              FI.flatten()[::25]):
+            dic = {"amplitude": amp,
+                   "similarity": cont,
+                   "gradient": grad,
+                   "intercept": inter,
+                   "FI": fi}
+                
+            output["data"].append(dic)
             
         self.response.out.write(json.dumps(output))
 
@@ -55,10 +84,20 @@ class vDHandler(Viz):
 
     def get(self):
 
-        data = np.random.randn(3, 50, 50).tolist()
-
+        attr1_id = self.request.get("attr1")
+        attr2_id = self.request.get("attr2")
+        
+        attr1 = np.flipud(np.nan_to_num(np.load(self.file_dict[attr1_id]))).T
+        attr2 = np.flipud(np.nan_to_num(np.load(self.file_dict[attr2_id]))).T
+        
+        output = {}
+        output["attr1"] = (attr1 / np.amax(np.abs(attr1))).tolist()
+        output["attr2"] = attr2.tolist()
+        output["cbar1"] = np.linspace(-1, 1, 10).tolist()
+        output["cbar2"] = (1 - np.linspace(0, .5, 10)).tolist()[::-1]
+   
         self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write(json.dumps(data))
+        self.response.out.write(json.dumps(output))
 
 
 app = webapp2.WSGIApplication([('/', MainHandler),
--- static/js/app.js
+++ static/js/app.js
@@ -12,156 +12,304 @@ app.config(['$interpolateProvider', function($interpolateProvider) {
 
 
 app.controller('controller', function ($scope, $http, $alert, $timeout) {
+
+
+  // Get the data
+  $http.get('/data').then(
+    function(resp){
+
+      $scope.vDPlot = initializeSeismic(resp.data.attr1,
+                                        resp.data.attr2);
+      $scope.drag = initializeDrag();
+      var svg = $scope.vDPlot._plot.svg();
+      $scope.cbar = initializeCBar(svg, resp.data.cbar1,resp.data.cbar2);
+      $scope.svg = svg;
+      $scope.scatterMatrix = initializeScatterMatrix(resp.data.attributes, 
+						     resp.data.data, svg);
+      $scope.scatter = initializeScatter(svg, resp.data.data);
+      
+      $scope.attributes = resp.data.attributes;
+      $scope.data = resp.data.data;
+
+      $scope.dummy_text = svg.append("text");
+    });
+
+  var initializeSeismic = function(attr1, attr2){
+
+    // Make the seismic plot
+    var vDPlot = g3.plot("#container")
+          .height(500)
+          .xTitle("spatial cross-section")
+          .yTitle("time [ms]")
+          .width(500)
+          .xTickFormat("")
+          .x2TickFormat("")
+          .y2TickFormat("")
+          .margin(20,1200,1200,40)
+	  .xDomain([0,50])
+	  .yDomain([0,50])
+          .draw();
+
+    $scope.colorScale = d3.scale.linear()
+      .range(['#FF0000', '#FFF', '#0000FF'])
+      .domain([-50, 0, 50]);
+
+    var data = colorScale(attr1, attr2);
+    var vd = g3.seismic(vDPlot, data, [0,0]).draw();
+    return vd;
+  };
+
+  var initializeScatter = function(canvas, data){
+
+    canvas.append('g')
+      .attr("id", 'scatter')
+      .attr("transform", "translate(625,200)");
     
-    $scope.scatDirty = false;
-    $scope.vDDirty = false;
+    // Make the first scatter plot
+    var s1Plot = g3.plot('#scatter')
+	  .height(275.0)
+	  .xDomain([-1,1])
+	  .yDomain([-1,1])
+          .xTitle('amplitude')
+          .yTitle('similarity')
+	  .width(275)
+	  .xTickFormat("")
+	  .x2TickFormat("")
+	  .y2TickFormat("")
+	  .yTickFormat("")
+	  .margin(20,40,40,40)
+	  .draw();
 
-    // Get the data
-    $http.get('/data').then(
-	function(resp){
+    var scatter = g3.scatter(s1Plot, data).draw('similarity', 'amplitude');
+    return scatter;
+  };
 
-	    $scope.vDPlot = initializeSeismic();
-	    var svg = $scope.vDPlot.svg()
-
-	    $scope.scatterMatrix = initializeScatterMatrix(resp.data.attributes, 
-							   resp.data.data, svg);
-	    $scope.scatter = initializeScatter(svg, resp.data.data);
-
-	    $scope.attributes = resp.data.attributes;
-	    $scope.data = resp.data.data;
-	    $scope.svg = svg;
-
-	});
-
-
-
-    $scope.colorScale = d3.scale.linear().domain([-1,0,1])
-	.range(['#FF0000', '#FFF', '#0000FF']);
+  var initializeScatterMatrix = function(attributes, data, canvas){
 
 
-    var initializeSeismic = function(){
+    var plots = [];
 
-	// Make the seismic plot
-	var vDPlot = g3.plot("#container")
-            .height(500)
-            .xTitle("spatial cross-section")
-            .yTitle("time [ms]")
-            .width(500)
-            .xTickFormat("")
-            .x2TickFormat("")
-            .y2TickFormat("")
-            .margin(20,1200,1200,40)
-	    .xDomain([0,50])
-	    .yDomain([0,50])
-            .draw();
-
-	return vDPlot;
+    var scopeFunc = function(primary_attr, secondary_attr){
+      var prime = primary_attr;
+      var second = secondary_attr;
+      var updateScatter = function(){
+	$scope.scatter._plot.yTitle(prime).xTitle(second);
+	$scope.scatter.reDraw(prime, second);
+	$scope.scatter._plot.getElement('yTitle').text(prime);
+	$scope.scatter._plot.getElement('xTitle').text(second);
+      };
+      return updateScatter;
     };
 
-   var initializeScatter = function(canvas, data){
 
-       canvas.append('g')
-	   .attr("id", 'scatter')
-	   .attr("transform", "translate(700,0)");
+    canvas.append('g')
+      .attr("id", 'small_multiples')
+      .attr("transform", "translate(700,0)");
+    
+    for(var i=0; i<attributes.length; i++){
+      var primary_attr = attributes[i];
+      
+      for(var j=1; j< attributes.length - i; j++){
+	var secondary_attr = attributes[i+j];
+	var y2Title = "";
+	var xTitle = "";
+	var yMarg = 20;
+
+	if(i===0){
+	  xTitle = secondary_attr;
+	};
+	if((i+j)===(attributes.length-1)){
+	  y2Title = primary_attr;
+	  yMarg = 30;
+	};
+
+	var yPos = i * 60 + 20; 
+	var xPos = (i + j) * 60 + 20;
+	var plot =  g3.plot('#small_multiples')
+	      .height(50)
+	      .xDomain([-1,1])
+	      .yDomain([-1,1])
+	      .width(50)
+	      .y2Title(y2Title)
+	      .xTitle(xTitle)
+	      .xTickFormat("")
+	      .x2TickFormat("")
+	      .y2TickFormat("")
+	      .yTickFormat("")
+	      .margin(yPos,20, yMarg, xPos)
+	      .draw();
 	
-	// Make the first scatter plot
-       var s1Plot = g3.plot('#scatter')
-	   .height(250)
-	   .xDomain([-3,3])
-	   .yDomain([-3,3])
-	   .width(250)
-	   .xTickFormat("")
-	   .x2TickFormat("")
-	   .y2TickFormat("")
-	   .yTickFormat("")
-	   .margin(20,40,40,40)
-	   .draw();
-
-       var scatter = g3.scatter(s1Plot, data);
-       return scatter;
-   };
-
-    var initializeScatterMatrix = function(attributes, data, canvas){
+	plots.push(g3.scatter(plot, data).draw(primary_attr, 
+					       secondary_attr));
 
 
-	var plots = [];
+	plot.svg().attr("cursor", "copy")
+	  .on("click", scopeFunc(primary_attr, secondary_attr));
 
-	var scopeFunc = function(primary_attr, secondary_attr){
-	    var prime = primary_attr;
-	    var second = secondary_attr;
-	    var updateScatter = function(){
-		$scope.scatter._plot.xTitle(prime).yTitle(second);
-		if($scope.scatDirty){
-		    $scope.scatter.reDraw(prime, second);
-		    $scope.scatter._plot.getElement('xTitle').text(prime);
-		    $scope.scatter._plot.getElement('yTitle').text(second);
-		    } else{
-			$scope.scatter.draw(prime, second);
-			$scope.scatDirty = true;
-			$scope.scatter._plot.setTitles()
-		    };
-
-	
-		
-	    }
-	    return updateScatter;
-	}
-
-
-	canvas.append('g')
-	    .attr("id", 'small_multiples')
-	    .attr("transform", "translate(500,100)");
-	
-	for(var i=0; i<attributes.length; i++){
-	    var primary_attr = attributes[i];
-	    
-	    for(var j=1; j< attributes.length - i; j++){
-		var secondary_attr = attributes[i+j];
-		var yTitle = "";
-		var xTitle = "";
-		var yMarg = 20;
-
-		if(i===0){
-		    yTitle = secondary_attr;
-		};
-		if((i+j)===(attributes.length-1)){
-		    xTitle = primary_attr;
-		    yMarg = 30;
-		};
-
-		var xPos = i * 60 + 20; 
-		var yPos = (i + j) * 60 + 20;
-		var plot =  g3.plot('#small_multiples')
-		    .height(50)
-		    .xDomain([-3,3])
-		    .yDomain([-3,3])
-		    .width(50)
-		    .yTitle(yTitle)
-		    .x2Title(xTitle)
-		    .xTickFormat("")
-		    .x2TickFormat("")
-		    .y2TickFormat("")
-		    .yTickFormat("")
-		    .margin(yPos,20, yMarg, xPos)
-		    .draw();
-		
-		plots.push(g3.scatter(plot, data).draw(primary_attr, 
-						       secondary_attr));
-
-
-		plot.svg().attr("cursor", "copy")
-		    .on("click", scopeFunc(primary_attr, secondary_attr));
+        
+        plot.getElement('xTitle').attr("cursor", "grab")
+          .data([secondary_attr])
+          .call($scope.drag);
+        plot.getElement('y2Title').attr("cursor", "grab")
+          .data([primary_attr])
+          .call($scope.drag);
 
 	
-		
-	    }
-	}
-	return plots;
+      }
+    }
+    return plots;
+  };
+
+
+  var colorScale = function colorScale(attr1, attr2){
+
+    var cmap = [];
+    for(var i=1; i < attr1.length; i++){
+      var row = [];
+      for(var j=1; j < attr1[0].length; j++){
+        
+        var hsl = d3.hsl($scope.colorScale(attr1[i][j]));
+        hsl.l = hsl.l * attr2[i][j];
+        row.push(hsl.toString());
+        
+        
+      }
+      cmap.push(row);
+    }
+    return [cmap];
     };
     
+  var cBarData = function initializeCbar(cmap1, cmap2){
+
+    var cmap = [];
+    for(var i=1; i < cmap1.length; i++){
+      var row = [];
+      for(var j=1; j < cmap2.length; j++){
+        
+        var hsl = d3.hsl($scope.colorScale(cmap1[j]));
+        hsl.l = hsl.l * cmap2[i];
+        row.push(hsl.toString());
+        
+        
+      }
+      cmap.push(row);
+    };
+
+    return cmap;
+  };
+
+  var initializeCBar = function(svg, cmap1, cmap2){
+
+    var plot = g3.plot("#container")
+          .height(75)
+          .width(75)
+          .xTickFormat("")
+          .x2Title("similarity")
+          .yTitle("amplitude")
+          .x2TickFormat("")
+          .yTickFormat("")
+          .y2TickFormat("")
+          .margin(0,0,0,550)
+	  .xDomain([0,10])
+	  .yDomain([0,10])
+          .svg(svg)
+          .draw();
+
+    var cmap = cBarData(cmap1, cmap2);
+    var cmap_plot = g3.canvas(plot, [cmap], [20,40]).draw();
+    
+ 
+    svg.append("rect").attr("class", "label-hint")
+      .attr("x", 550).attr("y", 75).attr("height", 25)
+      .attr("width", 75).attr("stroke", "green")
+      .attr("stroke-opacity", 0).attr("fill-opacity", 0)
+      .attr("pointer-events", "none");
+
+     
+    svg.append("rect").attr("class", "label-hint")
+      .attr("x", 530).attr("y", 0).attr("height", 75)
+      .attr("width", 75).attr("stroke", "green")
+      .attr("stroke-opacity", 0).attr("fill-opacity", 0)
+      .attr("pointer-events", "none");
+
+    // Interactions
+    plot.getElement("x2Title")
+      .attr("class", "cmap_label").data([plot.x2Title()]);
+    plot.getElement("yTitle")
+      .attr("class", "cmap_label").data([plot.yTitle()]);
+
+    svg.selectAll('.cmap_label')
+      .on("mouseenter", function(){
+        if($scope.drag_active){
+          d3.select(this).text($scope.dummy_text.text())
+            .attr("font-size", "24px");
+        }
+      })
+      .on("mouseleave", function(d){
+         d3.select(this).text(d)
+          .attr("font-size", "12px");
+      })
+      .on("mouseup", function(){
+         if($scope.drag_active){
+           d3.select(this).attr("font-size", "12px")
+             .data([$scope.dummy_text.text()]);
+
+           vDUpdate();
+         }
+      });
+
+    return cmap_plot;
+  };
+
+  var initializeDrag = function(){
+
+    $scope.drag_active = false;
+
+    var drag = d3.behavior.drag();
+    drag.on("dragstart", function(d){
+      $scope.drag_active = true;
+      $scope.svg.selectAll('.label-hint').attr("stroke-opacity", 1);
+      $scope.dummy_text.text(d)
+        .attr("pointer-events", "none")
+	.attr("x", d3.mouse($scope.svg.node())[0])
+	.attr("y", d3.mouse($scope.svg.node())[1]);});
+    
+    drag.on("drag", function(){
+      $scope.dummy_text
+	.attr("x", d3.mouse($scope.svg.node())[0])
+	.attr("y", d3.mouse($scope.svg.node())[1]);});
+    
+    drag.on("dragend", function(){
+      $scope.drag_active = false;
+      $scope.dummy_text.text('');
+      $scope.svg.selectAll('.label-hint').attr("stroke-opacity", 0);
+    });
+
+   return drag;
+  };
+    
+  var vDUpdate = function vDUpdate(){
 
 
-});
+    var attr2 = $scope.cbar._plot.getElement('x2Title').text();
+    var attr1 = $scope.cbar._plot.getElement('yTitle').text();
+
+    $http.get('/vd_data?attr1=' + attr1 +'&attr2=' + attr2)
+      .then(function(resp){
+
+        var data = colorScale(resp.data.attr1, resp.data.attr2);
+
+        $scope.vDPlot.reDraw(data);
+        
+
+        var cmap = cBarData(resp.data.cbar1, resp.data.cbar2);
+
+        $scope.cbar.reDraw([cmap]);
+      });
+
+  };
+}
+);
 
 // <-- HELPER FUNCTIONS --> //
 // Take two numbers and return the abs max of the two
